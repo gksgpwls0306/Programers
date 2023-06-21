94f18486376000e28a025747b0236888c7ceab53
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-06-21 11:45:53 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = {};
+        answer = new int[(end-start+1)];
+        for(int i = 0;i<=(end-start);i++){
+            answer[i]=start+i;
+        }
+        return answer;
+    }
+}
