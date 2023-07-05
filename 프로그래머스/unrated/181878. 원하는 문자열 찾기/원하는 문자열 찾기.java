e11cd243c0388ff9a3fc130d4d176216dd2b8b2c
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-07-05 11:45:36 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 7.92 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        if(myString.toLowerCase().contains(pat.toLowerCase()))
+            answer  = 1; 
+        return answer;
+    }
+}
