1a768bad5817db2b6ca75bcecea82afce762baaf
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-07-14 12:08:01 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i = 0; i<num_list.length; i++){
+            if(num_list[i]<0)
+                return i;
+        }
+        return -1;
+    }
+}
