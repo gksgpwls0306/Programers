cd7a057d771b1005eaa5ac7597df89b82a5a1ad2
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-08-25 09:59:26 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 2;
+        if(str1.contains(str2))
+            return 1;
+        return answer;
+    }
+}
