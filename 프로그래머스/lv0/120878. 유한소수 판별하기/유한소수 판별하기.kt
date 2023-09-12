3978d7bb6bc389cc4532bbcad0a6b6b1b4af9e6c
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-09-12 21:10:12 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.17 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun solution(a: Int, b: Int): Int {
+        var newB = b / gcd(a, b)
+        var bb = 12%36
+        print(bb)
+        
+        while (newB != 1) {
+            newB /= if (newB % 2 == 0) {
+                2
+            } else if (newB % 5 == 0) {
+                5
+            } else {
+                return 2
+            }
+        }
+        return 1
+    }
+
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+}
