d6c56120e78b94af817da08cd7ce0f2f68119f36
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-09-23 20:44:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.12 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 1; i <= n; i++){
+            answer += i % 2 == 0 ? "박" : "수" ;
+        }
+        return answer;
+    }
+}
