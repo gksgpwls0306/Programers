8238974ab3a12b65d4f4fbb413496ad30c4d3bb1
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2023-10-07 16:54:18 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.85 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,36 +1,76 @@
+import java.text.*;
 import java.util.*;
+// import java.util.Calender;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         int[] answer = {};
-        int month = 28;
-        ArrayList<Integer> list = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
         // 약관
         for(String s : terms){
             String[] ss = s.split(" ");
-            map.put(ss[0],Integer.valueOf(ss[1])*month);
+            map.put(ss[0],Integer.valueOf(ss[1]));
+            // System.out.println(map);
         }
-        // today
-        int tday = Integer.valueOf(today.split("\\.")[0])*month*12+Integer.valueOf(today.split("\\.")[1])*month+Integer.valueOf(today.split("\\.")[2]);
+        //개인정보
+        // HashMap<String, String> pri = new HashMap<>();
+        // for(String s : privacies){
+        //     String[] ss = s.split(" ");
+        //     pri.put(ss[0],ss[1]);
+        // }
         
-        //변환 및 비교
-        for(int i = 0; i < privacies.length; i++){
-            String s = privacies[i].split(" ")[0];
-            String a = privacies[i].split(" ")[1];
-            int dday = Integer.valueOf(s.split("\\.")[0])*month*12+Integer.valueOf(s.split("\\.")[1])*month+Integer.valueOf(s.split("\\.")[2]);
-            System.out.println(map.get(a));
-            dday += map.get(a);
+        // ArrayList<String>[][] list = new ArrayList[privacies.length][2];
+        // for(int i = 0; i < privacies.length; i++){
+        //     // list[i] = new ArrayList<String>(); 
+        //     String[] ss = privacies[i].split(" ");
+        //     list[i].add(ss[0]);
+        //     list[i].add(ss[1]);
+        //     System.out.println(privacies[i]);
+        //     System.out.println(ss[0]);
+        //     // list[i][0].add(privacies[i].split(" "));
+        // }
+
+        // for(String s : privacies){
+        //     System.out.println(s);
+        // }
+        
+        // for(String s : privacies){
+        //     System.out.println(s);
+        // }
+        
+        SimpleDateFormat sim = new SimpleDateFormat("yyyy.MM.dd");
+        try{
+            Date tday = sim.parse(today);
+        }catch(Exception e){}
+        Calendar cal = Calendar.getInstance();
+        for(String s : privacies){
+            try{
+                // String day = s.split(" ")[0];
+                Date date = sim.parse(s.split(" ")[0]);
+                cal.setTime(date);
+                int  m = map.get(s.split(" ")[1]);
+                if(m >= 12){
+                    cal.add(Calendar.YEAR, 1);
+                    cal.add(Calendar.MONTH, m-12);
+                    cal.add(Calendar.DATE, -1);
+                    // System.o
+                }else{
+                    cal.add(Calendar.MONTH, m);
+                }
+                System.out.println(sim.format(cal.getTime()));
+            }catch(Exception e){}
             
-            if(tday >= dday){
-                list.add(i+1);
-            }
         }
-        answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-      
+        // cal.
+        
+        
+        
+        
         return answer;
     }
+    
+    //String to Cal
+//     void toCal(String day) throws Exception{
+        
+//     }
 }
