bf99fe377a53ea929f53e189f6d7cb82bb5615ee
gksgpwls0306 <65875927+gksgpwls0306@users.noreply.github.com>
2024-01-10 14:04:53 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean[] visited;
+    public int answer;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return answer;
+    }
+
+    public void dfs(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true; 
+                dfs(depth + 1, k - dungeons[i][1], dungeons); 
+                visited[i] = false; 
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+}
